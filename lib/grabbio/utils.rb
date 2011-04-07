3d60d3abf5e01f357fe01783e386ef2e1c2c0574
Gary Rennie <webmaster@gazler.com>
2011-04-07 16:17:31 +0100

    test

--- lib/grabbio/utils.rb
+++ lib/grabbio/utils.rb
@@ -3,6 +3,22 @@ require 'openssl'
 
 module Grabbio
   module Utils
+
+    def parameters_to_string(parameters)
+      first = true
+      args = ""
+      parameters.each do |k,v|
+        if first
+          first = false
+          args += "?"
+        else
+          args += "&"
+        end
+        args += "#{k}=#{v}"
+      end
+      args
+    end
+
     def sign_request(secret_key, parameters)
        Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), secret_key,parameters)).chomp.gsub(/\n/,'')
     end
