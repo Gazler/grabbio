8e2b3e2f21183da5568d5f00e4662248a6a6cee4
Gary Rennie <webmaster@gazler.com>
2011-04-07 16:44:51 +0100

    test

--- lib/grabbio/client.rb
+++ lib/grabbio/client.rb
@@ -29,7 +29,7 @@ module Grabbio
       parameters = parameters_to_string(opts)
       parameters += "&server_time=#{Time.now.to_i}"
       parameters += "&token=#{@api_key}"
-      parameters += "&hash=#{sign_request(@api_secret, parameters)}"
+      parameters += "&hash=#{sign_request(@api_secret, parameters.slice(0))}"
       url = API_URL+API_VERSION+"/videos.json#{parameters}"
       p url
       make_request(url)
